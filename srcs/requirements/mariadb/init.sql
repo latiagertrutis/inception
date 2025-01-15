71cf97cf85d29c521c9a251d48a79d3a76d74859
mateo <teorodrip@posteo.net>
2025-01-15 17:43:16 +0100

    move things

--- srcs/requirements/mariadb/init.sql
+++ srcs/requirements/mariadb/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE wordpress;
 USE wordpress;
+-- Format is user@hostname.network_name. Network name appears here since we are using docker.
 CREATE USER 'wpuser'@'inception-wordpress.inception' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'wpuser'@'inception-wordpress.inception' WITH GRANT OPTION;
