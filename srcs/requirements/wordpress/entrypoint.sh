9a960a062fa544160259d4597d6e807f502d0ab3
mateo <teorodrip@posteo.net>
2025-02-19 15:26:35 +0100

    setup adminer

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -6,6 +6,7 @@ set -e
 if [ -z "$(ls -A "$WORDPRESS_DIR")" ]; then
     cp -r /wordpress/. "$WORDPRESS_DIR"
     cp /wp-config.php "$WORDPRESS_DIR/"
+    cp /adminer-4.8.1.php "$WORDPRESS_DIR/"
     mkdir -p "$WORDPRESS_DIR/wp-content/mu-plugins"
     cp -r /redis-cache/. "$WORDPRESS_DIR/wp-content/mu-plugins/"
     # Make wp-contet owned by wordpress so plugins can write
